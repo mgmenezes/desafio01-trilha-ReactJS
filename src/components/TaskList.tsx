d6378cebe0d5d7599122d485cbf17d48b4180932
mgmenezes <mgmenezes>
2021-03-20 19:44:16 -0300

    Desafio Completo

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -4,6 +4,8 @@ import '../styles/tasklist.scss'
 
 import { FiTrash, FiCheckSquare } from 'react-icons/fi'
 
+//import nextId from "react-id-generator";
+
 interface Task {
   id: number;
   title: string;
@@ -16,14 +18,30 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete:false,
+    }
+    setTasks(theLastState=>[...theLastState,newTask])
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const tasksComplete = tasks.map(task => task.id == id ? {
+        ...task,
+        isComplete : !task.isComplete
+    }: task)
+    setTasks(tasksComplete)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filter = tasks.filter(task => task.id != id)
+    setTasks(filter)
   }
 
   return (
